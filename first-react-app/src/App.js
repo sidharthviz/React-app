1c2be9519f85b1be4afb1724eb391e47e1260330
Siddharth verma <990.siddharthverma@gmail.com>
2023-01-16 03:16:16 +0530

    Changed

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,12 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import MovieCard from './MovieCard';
 
 import './App.css';
+
+import SearchIcon from './search.svg';
 
 //e52f2d97
 const API_URL = 'http://www.omdbapi.com?apikey=e52f2d97';
 
+const movie1 = {
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYWM0MDI1ZmItZTYzNi00ZWVlLTg5MTctNzllNjY2ZTI3NDhhXkEyXkFqcGdeQXVyNDk5MjA2MQ@@._V1_SX300.jpg",
+"Title": "Reign of Judges: Title of Liberty - Concept Short",
+"Type" : "movie",
+"Year": "2018",
+"imdbID": "tt4275958"
+}
+
 
 const App = () => {
+
+    const  [movies, setMovies] = useState([]);
   
   const searchMovie = async (title) => {
     const response = await fetch(`${API_URL}&s={title}`);
@@ -27,8 +41,31 @@ const App = () => {
             value='Zindagi Na Milegi Dobara'
             onChange={() => {}}
             />
+            <img
+            src={SearchIcon}
+            alt="search"
+            onChange={() => {}} 
+            />
+            </div>
 
-        </div>
+            {
+                movies?.length > 0 
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => {
+                            <MovieCard movie={movie}/>
+                        })}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                     </div>   
+                )
+            }
+
+            <div className="container">
+             <MovieCard movie1={movies[0]} />
+         </div>
         </div>
     )
 }
